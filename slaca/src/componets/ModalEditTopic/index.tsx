5e96dbb0ec6d647e9920579c17bf7e0397f1cbc2
Matheus Pires <matheuspdsousa@gmail.com>
2022-04-30 17:40:50 -0300

    Finalizando Context de edição de tópicos e atualização dos tópicos ao atualizar

--- slaca/src/componets/ModalEditTopic/index.tsx
+++ slaca/src/componets/ModalEditTopic/index.tsx
@@ -4,39 +4,52 @@ import { ChangeLanguage } from '../../context/ChangeLanguege';
 import { EditTopicContext } from '../../context/EditTopic'
 import { BoxInputs, Container } from './style';
 import { IoCloseSharp } from 'react-icons/io5'
+import { TopicContext } from '../../context/CreateTopics';
 
 export function ModalEdit(){
     const { language } = useContext(ChangeLanguage);
-    const { setShowEditModal, showEditModal } = useContext(EditTopicContext);
+    const { allTopics, setAllTopics } = useContext(TopicContext)
+    const { setShowEditModal, showEditModal, setContent, content, setSubject, subject, idTopic } = useContext(EditTopicContext);
 
+    type Topic = {subject: string, content: string}[];
 
+    function handleEditTopic(e: any){
+        e.preventDefault();
+        let topic: Topic = allTopics
+        console.log(topic)
+        topic[idTopic] = { subject: subject, content: content}
+        localStorage.setItem('topic', JSON.stringify(topic));
+        const getTopics = localStorage.getItem('topic');
+        const newTopics = getTopics && JSON.parse(getTopics);
+        setAllTopics(newTopics);
+        setShowEditModal(false);
+    }
 
     return(
+        <Modal
+            isOpen={showEditModal}
+            ariaHideApp={false}
+            onRequestClose={() => setShowEditModal(false)}
+            className={'modal'}
+            overlayClassName={'overlay-modal'}
+        >
+            <Container onSubmit={handleEditTopic}>
+                <h2>Edite seu Tópico</h2>
+                <button className='close-modal' onClick={() => setShowEditModal(false)}><IoCloseSharp/></button>
+                <BoxInputs>
+                    <div>
+                        <label htmlFor="assunt">{language? 'Assunt' : 'Assunto'}</label>
+                        <input type="text" name="assunt" id="assunt" value={subject} onChange={(e) => setSubject(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label htmlFor="content">{language? 'Content' : 'Conteúdo'}</label>
+                        <textarea name="content" id="content" value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+                    </div>
+                </BoxInputs>
 
-            <Modal
-                isOpen={showEditModal}
-                ariaHideApp={false}
-                onRequestClose={() => setShowEditModal(false)}
-                className={'modal'}
-                overlayClassName={'overlay-modal'}
-            >
-                <Container>
-                    <h2>Edite seu Tópico</h2>
-                    <button className='close-modal' onClick={() => setShowEditModal(false)}><IoCloseSharp/></button>
-                    <BoxInputs>
-                        <div>
-                            <label htmlFor="assunt">{language? 'Assunt' : 'Assunto'}</label>
-                            <input type="text" name="assunt" id="assunt"/>
-                        </div>
-                        <div>
-                            <label htmlFor="content">{language? 'Content' : 'Conteúdo'}</label>
-                            <textarea name="content" id="content"></textarea>
-                        </div>
-                    </BoxInputs>
-
-                    <button className='btn-final-form'>Finalizar Edição</button>
-                </Container>
-               
-            </Modal>
+                <button className='btn-final-form' type='submit'>Finalizar Edição</button>
+            </Container>
+            
+        </Modal>
     )
 }
--- slaca/src/context/EditTopic.tsx
+++ slaca/src/context/EditTopic.tsx
@@ -1,34 +1,55 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, MutableRefObject, ReactNode, useContext, useRef, useState } from "react";
 import { TopicContext } from "./CreateTopics";
 
 interface EditTopicContextProvider {
     children: ReactNode
 }
 
+interface Topic {
+    subject: string,
+    content: string,
+}
+
 interface EditTopicContextProps {
     handleOpenModal: (e: any) => void,
     setShowEditModal: (newState: boolean) => void,
     showEditModal: boolean,
+    setContent: (newState: string) => void;
+    content: string;
+    setSubject: (newState: string) => void;
+    subject: string;
+    idTopic: number
 }
 
 const initialValue = {
     handleOpenModal: () => {},
     setShowEditModal: () => {},
     showEditModal: false,
+    setContent: () => {},
+    content: '',
+    subject: '',
+    setSubject: () => {},
+    idTopic: 0,
 }
 export const EditTopicContext = createContext<EditTopicContextProps>(initialValue);
 
 export const EditTopicContextProvider = ({children}: EditTopicContextProvider) => {
-    const { allTopics, setAllTopics } = useContext(TopicContext);
-    const [showEditModal, setShowEditModal] = useState(false)
+    const { allTopics } = useContext(TopicContext);
+    const [showEditModal, setShowEditModal] = useState(false);
+    const [content, setContent] = useState(initialValue.content)
+    const [subject, setSubject] = useState(initialValue.subject)
+    const [idTopic, setIdTopic] = useState(initialValue.idTopic)
 
     function handleOpenModal(e: any){
+        const topic: Topic = allTopics[Number(e.target.dataset.id)];
+        setIdTopic(Number(e.target.dataset.id));
+        setContent(topic.content);
+        setSubject(topic.subject);
         setShowEditModal(true);
-        console.log(allTopics)
     }
 
     return(
-        <EditTopicContext.Provider value={{ handleOpenModal, setShowEditModal, showEditModal }}>
+        <EditTopicContext.Provider value={{ handleOpenModal, setShowEditModal, showEditModal, content, setContent, subject, setSubject, idTopic }}>
             {children}
         </EditTopicContext.Provider>
     )
